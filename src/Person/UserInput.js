7d2701b0acbd8800232c5382c274c8a7403c1c8a
Prashant Bhat <prashantbhat84@gmail.com>
2019-02-08 23:42:11 +0530

    two way binding final

--- src/Person/UserInput.js
+++ src/Person/UserInput.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./UserInput.css";
+import { className } from "postcss-selector-parser";
 
 const input = props => {
   return (
-    <div>
+    <div className="UserInput">
       <input type="text" onChange={props.changed} value={props.username} />
     </div>
   );
